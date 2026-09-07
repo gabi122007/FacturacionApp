80a20c3be309f1e54b9469f99086b74abc66ab9a
gabi122007 <gabiespr07@gmail.com>
2026-09-07 16:17:48 -0600

    update Producto

--- src/main/java/ni/edu/uam/facturacionapp/model/Producto.java
+++ src/main/java/ni/edu/uam/facturacionapp/model/Producto.java
@@ -1,4 +1,21 @@
 package ni.edu.uam.facturacionapp.model;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+@Data
 public class Producto {
+    private Integer id;
+    private String codigo;
+    private String nombre;
+    private String categoria;
+    private BigDecimal precio;
+    private int exsistencia;
+    private String rutaimagen;
+    private boolean activo;
 }
