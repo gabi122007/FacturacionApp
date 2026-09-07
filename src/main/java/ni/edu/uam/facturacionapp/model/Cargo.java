76cd8431eeed4bf53565116448f9d2bc6fe4b693
gabi122007 <gabiespr07@gmail.com>
2026-09-07 16:18:47 -0600

    update Cargo

--- src/main/java/ni/edu/uam/facturacionapp/model/Cargo.java
+++ src/main/java/ni/edu/uam/facturacionapp/model/Cargo.java
@@ -1,4 +1,15 @@
 package ni.edu.uam.facturacionapp.model;
 
+import lombok.*;
+
+@Data
+@Getter
+@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class Cargo {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+
 }
