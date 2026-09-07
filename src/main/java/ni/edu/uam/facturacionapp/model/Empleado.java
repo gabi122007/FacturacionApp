4df82047bb182a2cfa5943afcbed3fd066a88b92
gabi122007 <gabiespr07@gmail.com>
2026-09-07 16:22:33 -0600

    update empleado

--- src/main/java/ni/edu/uam/facturacionapp/model/Empleado.java
+++ src/main/java/ni/edu/uam/facturacionapp/model/Empleado.java
@@ -1,4 +1,19 @@
 package ni.edu.uam.facturacionapp.model;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Setter
+@AllArgsConstructor
+@Data
+@Getter
 public class Empleado {
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaNac;
+    private boolean activo;
 }
