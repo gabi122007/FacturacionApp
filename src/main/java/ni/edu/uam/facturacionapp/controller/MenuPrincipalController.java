cee810bcf3df553a9b4538ae7c98393ffc93ebb6
gabi122007 <gabiespr07@gmail.com>
2026-09-25 16:04:17 -0600

    Update MenuPrincipalController.java

--- src/main/java/ni/edu/uam/facturacionapp/controller/MenuPrincipalController.java
+++ src/main/java/ni/edu/uam/facturacionapp/controller/MenuPrincipalController.java
@@ -1,4 +1,29 @@
 package ni.edu.uam.facturacionapp.controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import ni.edu.uam.facturacionapp.util.SceneManager;
+import java.io.IOException;
+
 public class MenuPrincipalController {
+    @FXML
+    private void abrirProductos() {
+        try {
+            SceneManager.abrirVentana(
+                    "/ni/edu/uam/facturacion/fxml/producto-view.fxml",
+                    "Gestión de productos");
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR,
+                    "No fue posible abrir Productos.").showAndWait();
+        }
+    }
+
+    @FXML
+    private void salir() {
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION,
+                "¿Desea cerrar la aplicación?", ButtonType.OK, ButtonType.CANCEL);
+        if (a.showAndWait().orElse(ButtonType.CANCEL) == ButtonType.OK)
+            Platform.exit();
+    }
 }
