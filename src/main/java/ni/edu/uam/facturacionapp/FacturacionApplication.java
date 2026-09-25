1a2fda498d44c48db89880112a7a29d63f3b7213
gabi122007 <gabiespr07@gmail.com>
2026-09-25 15:43:20 -0600

    feat: agregar clase FacturacionApplication

---
+++ src/main/java/ni/edu/uam/facturacionapp/FacturacionApplication.java
@@ -0,0 +1,11 @@
+package ni.edu.uam.facturacionapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FacturacionApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
